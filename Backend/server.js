88eb4abca7b205d02be884345fa5fa488bd21929
Bhuvan <bhouvana@gmail.com>
2024-02-22 12:20:34 +0530

    updatedsever.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,4 +13,18 @@ mongoose.connect('mongodb+srv://bhouvana:FO5KzR5350F6kKPN@cluster0.0equbxr.mongo
     const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected';
     res.send(`Database connection status: ${dbStatus}`);
   });
-  
+  const express = require('express');
+const app = express();
+const port = process.env.PUBLIC_PORT || 3000; 
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 Server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
